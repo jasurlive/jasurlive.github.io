e8cc353cc9e410cd567eb3ee07adb0ac7a616b99
jasurgraduate <jasur@graduate.org>
2025-04-19 00:01:29 +0900

    edited configs and pages

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -5,41 +5,12 @@
       <h1>Jasur's trash projects</h1>
     </header>
 
-    <nav>
-      <button @click="view = 'home'">Home</button>
-    </nav>
-
     <main>
-      <component :is="currentView" />
+      <h1>Under maintenance</h1>
     </main>
   </div>
 </template>
 
-<script>
-import Home from "../components/Home.vue";
-import About from "../components/About.vue";
+<script></script>
 
-export default {
-  name: "App",
-  data() {
-    return {
-      view: "home",
-    };
-  },
-  computed: {
-    currentView() {
-      return this.view === "home" ? Home : About;
-    },
-  },
-};
-</script>
-
-<style scoped>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  text-align: center;
-}
-nav button {
-  margin: 0 10px;
-}
-</style>
+<style scoped></style>
--- src/vue/main.js
+++
@@ -1,4 +0,0 @@
-import { createApp } from "vue";
-import "../add/css/App.css";
-import App from "./App.vue";
-createApp(App).mount("#app");
