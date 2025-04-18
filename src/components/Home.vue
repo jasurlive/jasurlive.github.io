0e00e79e06019803ae5cc3ed53bdf22cfc4b33c9
jasurgraduate <jasur@graduate.org>
2025-04-18 23:17:29 +0900

    new vue project

---
+++ src/components/Home.vue
@@ -0,0 +1,13 @@
+<!-- src/components/Home.vue -->
+<template>
+  <div>
+    <h2>Home Page</h2>
+    <p>Welcome to the homepage!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
---
+++ src/vue/App.vue
@@ -0,0 +1,45 @@
+<!-- src/App.vue -->
+<template>
+  <div id="app">
+    <header>
+      <h1>Jasur's trash projects</h1>
+    </header>
+
+    <nav>
+      <button @click="view = 'home'">Home</button>
+    </nav>
+
+    <main>
+      <component :is="currentView" />
+    </main>
+  </div>
+</template>
+
+<script>
+import Home from "../components/Home.vue";
+import About from "../components/About.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      view: "home",
+    };
+  },
+  computed: {
+    currentView() {
+      return this.view === "home" ? Home : About;
+    },
+  },
+};
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+}
+nav button {
+  margin: 0 10px;
+}
+</style>
