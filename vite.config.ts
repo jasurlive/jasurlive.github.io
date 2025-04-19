9971752c04fc321307ac82867de37d70865fbbd0
jasurgraduate <jasur@graduate.org>
2025-04-19 12:41:23 +0900

    removed ts buildinfo file

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    extensions: [".js", ".ts", ".vue", ".css"], // Ensure .vue is included
+    extensions: [".js", ".ts", ".vue", ".css"],
   },
 });
